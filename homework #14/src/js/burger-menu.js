7e7fd7462c4d593869902a67abb335ee1dc8c3bf
Andr <andreymuzikant37@gmail.com>
2022-09-16 16:50:28 +0300

    hw#14/1

--- homework #14/src/js/burger-menu.js
+++ homework #14/src/js/burger-menu.js
@@ -1,6 +1,7 @@
-$(function() {
-	$(".nav-opener").on("click", function(e) {
-		e.preventDefault();
-		$("body").toggleClass("nav-active");
-	});
+$(function () {
+  $(".nav-opener").on("click", function (e) {
+    e.preventDefault();
+    $("body").toggleClass("nav-active");
+    $("body").toggleClass("lock");
+  });
 });
